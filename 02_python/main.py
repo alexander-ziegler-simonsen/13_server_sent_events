999feaf66daaffe72c8f6047b55eaa0ea0a5f5b1
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-03-19 16:38:41 +0100

    part 13 - recovered loss files

--- 02_python/main.py
+++ 02_python/main.py
@@ -3,15 +3,16 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import StreamingResponse
 from datetime import datetime
 import asyncio
+import random
 
 app = FastAPI()
 
+# this is used for setting up a folder for static files
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-def serve_root_page(request: Request):
+def server_root_page(request: Request):
     return templates.TemplateResponse("index.html", { "request": request })
-
 
 async def date_generator():
     while True:
@@ -19,7 +20,16 @@ async def date_generator():
         yield f"data: {now}\n\n"
         await asyncio.sleep(1)
 
-
 @app.get("/sse")
 def sse():
     return StreamingResponse(date_generator(), media_type="text/event-stream")
+
+async def kongfu_generator():
+    while True:
+        hits = [ "EEE-YA", "EII-YA", "EEIII!", "YA!", "oda oda oda oda oda !!!!" ]
+        yield f"data: {random.choice(hits)}\n\n"
+        await asyncio.sleep(1)
+
+@app.get("/kong_fu")
+def kongfu():
+    return StreamingResponse(kongfu_generator(), media_type="text/event-stream")
